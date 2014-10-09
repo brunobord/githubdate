d7006c44162e10472c7214e2557f38e26945e1cc
Bruno Bord <bruno@jehaisleprintemps.net>
2014-10-09 23:40:44 +0200

    githubdate: Update your PR statuses.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Githubdate
+
+## Use case
+
+You've got dozens of pull-requests on hold. Some of them are ready to be merged.
+But as a release manager, you need to warn everyone with "merge" rights **not
+to** merge any of these pull-requests. Because you're on a hot lava field, you
+have to stabilize things, etc.
+
+Anyway. Just push a "it's not cool to merge this" status. And then, when the
+situation is cooler, just send an update with a green light.
+
+## Install
+
+* clone this repository,
+* pip install it (preferrably in a virtualenv)
+
+## Configure
+
+Pick the [githubdate.ini.example](githubdate.ini.example) file and save it as
+``githubdate.ini``. Edit it to configure the different variables:
+``personal_access_token``, ``owner`` (github user account), target
+``repository``, ``target_url`` (optional), and the ``description`` (the message
+to be sent to the user).
+
+## Run
+
+    githubdate pause  # to pause your PRs
+    githubdate unpause  # to free your PRs from this pending status
---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+VERSION = '1.0.0'
+
+setup(
+    name='githubdate',
+    description="Send update statuses to your repository pull-requests.",
+    long_description="""
+==========
+githubdate
+==========
+
+You've got dozens of pull-requests on hold. Some of them are ready to be
+merged. But as a release manager, you need to warn everyone with "merge"
+rights **not to** merge any of these pull-requests. Because you're on a hot
+lava field, you have to stabilize things, etc.
+
+Anyway. Just push a "it's not cool to merge this" status. And then, when the
+situation is cooler, just send an update with a green light.
+
+License
+-------
+
+This is a MIT-licensed software, by Bruno Bord (c) 2014.
+
+    """,
+    url='https://github.com/brunobord/githubdate/',
+    author="Bruno Bord",
+    author_email='bruno@jehaisleprintemps.net',
+    license="MIT",
+    platforms='any',
+    version=VERSION,
+    install_requires=['requests', "click"],
+    scripts=['bin/githubdate']
+)
